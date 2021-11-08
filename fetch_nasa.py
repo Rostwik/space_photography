bfd34007aa3c53d899295b9ff2c9dc5b2c04324e
Rostislav <rostwik@gmail.com>
2021-11-08 23:10:58 +0300

    Added an error handler

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -1,13 +1,12 @@
 import datetime
-import time
 
 import requests
 import pathlib
 
+from tools import get_file_name
 
-def download_nasa_photos(nasa_api_token, delay_in_sending, directory_name):
-    time.sleep(delay_in_sending)
 
+def download_nasa_photos(nasa_api_token, directory_name):
     pathlib.Path(directory_name).mkdir(exist_ok=True)
 
     payloads = {'api_key': nasa_api_token}
@@ -23,5 +22,18 @@ def download_nasa_photos(nasa_api_token, delay_in_sending, directory_name):
         response = requests.get(image_url, params=payloads)
         response.raise_for_status()
         path_to_save = f'{directory_name}/{image_name}.png'
+        with open(path_to_save, 'wb') as file:
+            file.write(response.content)
+
+    payloads = {'api_key': nasa_api_token, 'count': 30}
+    response = requests.get('https://api.nasa.gov/planetary/apod', params=payloads)
+    response.raise_for_status()
+    urls_of_pictures = response.json()
+
+    for url_picture in urls_of_pictures:
+        response = requests.get(url_picture['url'])
+        response.raise_for_status()
+        filename_of_picture = get_file_name(url_picture['url'])
+        path_to_save = f'{directory_name}/{filename_of_picture[1]}'
         with open(path_to_save, 'wb') as file:
             file.write(response.content)
--- main.py
+++ main.py
@@ -1,41 +1,55 @@
-# @Space_photography_bot
 import os
 from os import listdir
+import random
+import time
 
+import requests
 import telegram
 from dotenv import load_dotenv
+from requests import HTTPError
+
 from fetch_nasa import download_nasa_photos
 from fetch_spacex import download_spacex_launch_photos
 
 
-def send_photos_to_telegram_group(bot, directory_name):
-    while True:
+def send_photos_to_telegram_group(bot, directory_name, delay):
+    time.sleep(delay)
 
-        pictures = listdir(directory_name)
+    pictures = listdir(directory_name)
+    random_picture = random.choice(pictures)
 
-        for picture in pictures:
-            photo = open(f'{directory_name}/{picture}', 'rb')
-            bot.send_photo(
-                chat_id='@whydoesapersonnotsitathome',
-                caption=f'{picture}',
-                photo=photo,
-            )
+    photo = open(f'{directory_name}/{random_picture}', 'rb')
+    bot.send_photo(
+        chat_id='@whydoesapersonnotsitathome',
+        caption=f'{random_picture}',
+        photo=photo,
+    )
 
 
 def main():
     bot = telegram.Bot(token=telegram_bot_token)
     pictures_directory = 'images'
-    flight_number = 45
+    flight_number = 46
 
     download_spacex_launch_photos(pictures_directory, flight_number)
-    download_nasa_photos(nasa_api_token, int(delay_in_sending), pictures_directory)
+    download_nasa_photos(nasa_api_token, pictures_directory)
 
-    send_photos_to_telegram_group(bot, pictures_directory)
+    while True:
+        send_photos_to_telegram_group(bot, pictures_directory, int(delay))
 
 
 if __name__ == '__main__':
     load_dotenv()
     nasa_api_token = os.getenv('NASA_API_TOKEN')
     telegram_bot_token = os.getenv('TELEGRAM_TOKEN')
-    delay_in_sending = os.getenv('DELAY_IN_SENDING')
-    main()
+    delay = os.getenv('DELAY_IN_SENDING')
+
+    try:
+        main()
+    except HTTPError as err:
+        if err.response.status_code == 404:
+            print('Запрашиваемый Вами адрес не существует.')
+        if err.response.status_code == 401:
+            print('Проверьте Ваш токен.')
+        if err.response.status_code == 500:
+            print('Сайт, куда Вы обращаетесь временно недоступен, попробуйте позже.')
